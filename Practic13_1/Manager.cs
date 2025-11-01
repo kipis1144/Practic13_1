d8ec35c81898340e4d6db8cb92ac347a1b5441ee
Kirill <kipis1144@gmail.com>
2025-11-01 23:15:54 +0300

    Добавьте файлы проекта.

---
+++ Practic13_1/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic13_1
+{
+    internal class Manager : Employee
+    {
+        private int teamSize;
+        public int TeamSize { 
+            get { return teamSize; }
+            set {
+                if (teamSize >= 0) { teamSize = value; }
+                else { teamSize = 0; }
+            }
+        }
+        public Manager(string name, string position, int salary, string acceptDate, int teamSize) : base(name, position, salary, acceptDate)
+        {
+            this.TeamSize = teamSize;
+        }
+
+        public void HoldMeeting() {
+            Console.WriteLine($"Менеджер {Name} провел собрание с командой из {TeamSize} человек");
+        }
+    }
+}
---
+++ Practic13_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic13_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Inna = new Employee("Инна", "Уборщица", 20000, "01.05.2023");
+            Manager Boris = new Manager("Борис", "Менеджер", 76000, "01.03.2022", 2);
+            Developer Artem = new Developer("Артем", "Разработчик", 130000, "27.10.2024", "C#");
+            Director Denis = new Director("Денис Игоревич", "CEO", 76000, "01.01.2022", "Разработка десктопного ПО");
+
+            Denis.AcceptBudget();
+            Boris.HoldMeeting();
+            Artem.WriteCode();
+            Artem.WriteCode();
+            Artem.WriteCode();
+
+        }
+    }
+}
